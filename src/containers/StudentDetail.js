642dcd88887e4910ae442b7ebc463783e74492a7
pamperboy <marc_smalbrugge@live.nl>
2018-01-25 16:51:38 +0100

    add style

--- src/containers/StudentDetail.js
+++ src/containers/StudentDetail.js
@@ -9,14 +9,15 @@ import { fetchStudents } from '../actions/students/fetch'
 
 import Paper from 'material-ui/Paper'
 
-import './BatchDetail.css'
+import './StudentDetail.css'
 
 class StudentDetail extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = { currentStudent: {},
-                   nextStudent: {}
+                   nextStudent: {},
+                   updated: false
                  }
   }
 
@@ -40,17 +41,39 @@ class StudentDetail extends PureComponent {
       currentStudent: this.props.students[index],
       nextStudent: this.props.students[
         index === this.props.students.length -1 ? 0 : index + 1
-      ]
+      ],
+      updated: true
     })
   }
 
+  giveGrade = (student) => {
+    if (student.evaluations.length > 0) {
+      return student.evaluations[0].evaluationGrade
+    } else {
+      return "NOGRADE"
+    }
+  }
+
+  insertAttribute = () => {
+    if (this.state.updated) {
+      return this.state.currentStudent.evaluations[0].evaluationGrade
+    }
+  }
+
   render() {
-    const { currentStudent, nextStudent } = this.state
+    const { currentStudent, nextStudent, updated } = this.state
     return (
       <div className="Batch">
 
-        <Paper className="paper">
-          <h4>{currentStudent.name}</h4>
+        <Paper className="paper studentHeader"
+          data-grade={this.insertAttribute()}
+          >
+          <div
+            className="profileImage"
+            style={{ backgroundImage: `url(${currentStudent.profileImage})` }}></div>
+            <div className="infoWrapper">
+              <h1>{currentStudent.name}</h1>
+            </div>
         </Paper>
 
       </div>
